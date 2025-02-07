3a5197c315b01352a87e7e62dd8915472f33064b
Alimedhat000 <alisensei000@gmail.com>
2025-02-07 19:18:39 +0200

    add emergency component with popup functionality and styling

---
+++ js/components/emergency-component.js
@@ -0,0 +1,24 @@
+let isEmergencyStop = false;
+
+$(document).ready(function () {
+  $("#emergencyStop").on("click", function () {
+    isEmergencyStop = !isEmergencyStop;
+
+    if (isEmergencyStop) {
+      $(".emergency-popup").addClass("active").removeClass("hidden").html(`
+                    <div class="emergency-popup-container">
+                      <h2 class="emergency-popup-title">Emergency Stop!</h2>
+                      <p>System has been stopped.</p>
+                      <button id="acknowledgeBtn">Acknowledge</button>
+                    </div>
+                `);
+
+      $("#acknowledgeBtn").on("click", function () {
+        $(".emergency-popup").removeClass("active").addClass("hidden");
+        isEmergencyStop = false;
+      });
+    } else {
+      $(".emergency-popup").removeClass("active").addClass("hidden");
+    }
+  });
+});
--- js/script.js
+++ js/script.js
@@ -3,3 +3,5 @@ import { loadComponent } from "./loader.js";
 loadComponent("status-component", "#status-component");
 loadComponent("control-component", "#controls-component");
 loadComponent("camera-component", "#camera-component");
+loadComponent("emergency-component", "#emergency-component");
+loadComponent("data-component", "#data-component");
